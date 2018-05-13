f60d60c267fd7c841efd3a8daafa1855c0c05dd5
lb <36658681+lucabarbieri@users.noreply.github.com>
2018-05-13 20:52:58 +0200

    Add files via upload

---
+++ lucabarbieri/making visible/testing/sketch.js
@@ -0,0 +1,59 @@
+var ctracker;
+
+var ec = new emotionClassifier();
+ec.init(emotionModel);
+var emotionData = ec.getBlank();
+
+function setup() {
+
+    // setup camera capture
+    var videoInput = createCapture();
+    videoInput.size(800, 600); //position of camera box
+    videoInput.position(0, 0);
+    videoInput.id("v");
+    var mv = document.getElementById("v");
+    mv.muted = true;
+
+
+    // setup canvas
+    var cnv = createCanvas(800, 600); // dimenion of the canvas (overlay)
+    cnv.position(0, 0);
+
+    // setup tracker
+    ctracker = new clm.tracker();
+    ctracker.init(pModel);
+    ctracker.start(videoInput.elt);
+    noStroke();
+}
+
+function draw() {
+    clear();
+
+    // darken video bg
+    fill(0,50); // darkness of overlay
+    rect(0,0,width,height); // dimension of the darkness overlay
+
+    fill(255);
+
+    var positions = ctracker.getCurrentPosition();
+    for (var i=0; i<positions.length -3; i++) {
+        ellipse(positions[i][0], positions[i][1], 2, 2); // draw a position of traker
+    }
+
+    var cp = ctracker.getCurrentParameters();
+    var er = ec.meanPredict(cp);
+
+    if (er) {
+        // andry=0, sad=1, surprised=2, happy=3
+        for (var i = 0;i < er.length;i++) {
+            rect(i * 110+20, height-80, 30, -er[i].value * 30);
+        }
+    }
+    text("ANGRY", 20, height-40);
+    text("SAD", 130, height-40);
+    text("SURPRISED", 220, height-40);
+    text("HAPPY", 340, height-40);
+    text("FEAR", 460, height-40); // not very good
+    text("DEGUSTED", 580, height-40); // not very good
+
+}
